d32f621dc7d27dca6691afc1b5851426dc331b27
David <dagoch@gmail.com>
2015-11-22 16:45:19 -0500

    Dagoch: trying to share moving sphere between networked instances

---
+++ Assets/Scripts/notused-RollSphere.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RollSphere : MonoBehaviour {
+	// THOUGHT THIS MIGHT BE NEEDED BUT IT ISN'T BECAUSE PHYSICS ALREADY MAKES THE SPHERE ROLL AWAY FROM
+	// YOU WHEN YOU BUMP IT
+
+	public float speed;   // use this as speed multiplier
+	private Rigidbody rb; // this is the object's rigidbody
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// this is where physics code goes
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		rb.AddForce (movement * speed);
+		
+	}
+
+}
